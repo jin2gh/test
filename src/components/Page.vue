69816e039e6fd673cbf0cb8f2ec6af193d09f117
jinji <jinjiting2690@gmail.com>
2021-04-26 19:52:48 +0800

    test: styl

---
+++ src/components/Page.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="page">
+    <div class="content">
+      <Content class="docs-en" slot-key="en" />
+      <Content class="docs-zh" slot-key="zh" />
+    </div>
+    <PageEdit />
+
+    <PageNav v-bind="{ sidebarItems }" />
+  </main>
+</template>
+
+<script>
+import PageEdit from '@theme/components/PageEdit.vue'
+import PageNav from '@theme/components/PageNav.vue'
+
+export default {
+  components: {
+    PageEdit,
+    PageNav
+  },
+  props: ['sidebarItems']
+}
+</script>
+
+<style lang="stylus">
+@import './Page.styl'
+</style>
